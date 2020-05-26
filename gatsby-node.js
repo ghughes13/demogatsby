4dd90c790338096e9733d1e22a7b70e6e4bfa88d
ghughes13 <guitarguy13@ymail.com>
2020-05-25 19:49:00 -0500

    Gatsby Site That Pulls Blogs From WP

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,39 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const { slash } = require(`gatsby-core-utils`)
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // query content for WordPress posts
+  const result = await graphql(`
+    query {
+      allWordpressPost {
+        edges {
+          node {
+            title
+            date
+            excerpt
+            id
+            slug
+            featured_media {
+              localFile {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const postTemplate = path.resolve(`./src/templates/BlogPost.js`)
+  result.data.allWordpressPost.edges.forEach(edge => {
+    createPage({
+      path: edge.node.slug,
+      component: slash(postTemplate),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
